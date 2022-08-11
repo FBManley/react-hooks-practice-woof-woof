deb878ab9eb927ad8e5ef71ceffb82fa1f4036ba
Jacob Manley <jbmanley45@gmail.com>
2022-08-11 10:26:40 -0700

    test

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function App() {
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
+      <div></div>
       <div id="dog-bar"></div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
